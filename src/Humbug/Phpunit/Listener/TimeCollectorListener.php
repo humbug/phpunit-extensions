2302dec6142773bbf6caf9307c96ef07d96f7139
Padraic Brady <padraic.brady@gmail.com>
2015-01-25 20:39:50 +0000

    Limit logging to file level suites (omit root parent or data provider children as duplicated data)

--- src/Humbug/Phpunit/Listener/TimeCollectorListener.php
+++ src/Humbug/Phpunit/Listener/TimeCollectorListener.php
@@ -15,6 +15,8 @@ use Humbug\Phpunit\Logger\JsonLogger;
 class TimeCollectorListener extends \PHPUnit_Framework_BaseTestListener
 {
 
+    protected $rootSuiteName;
+
     protected $currentSuiteName;
 
     protected $currentSuiteTime = 0;
@@ -26,6 +28,9 @@ class TimeCollectorListener extends \PHPUnit_Framework_BaseTestListener
 
     public function startTestSuite(\PHPUnit_Framework_TestSuite $suite)
     {
+        if (!isset($this->rootSuiteName)) {
+            $this->rootSuiteName = $suite->getName();
+        }
         $this->currentSuiteName = $suite->getName();
     }
 
@@ -41,6 +46,16 @@ class TimeCollectorListener extends \PHPUnit_Framework_BaseTestListener
 
     public function endTestSuite(\PHPUnit_Framework_TestSuite $suite)
     {
+        /**
+         * Skip logging of suites which are either:
+         *      a) The root suite containing other file level suites; or
+         *      b) End in ::testName which signify a data provider suite that
+         *          will already be getting logged by its parent suite.
+         */
+        if (preg_match("%\\:\\:[[:alnum:]]+$%", $suite->getName())
+        || $this->rootSuiteName == $suite->getName()) {
+            return;
+        }
         $this->logger->logTestSuite(
             $suite->getName(),
             $this->currentSuiteTime
